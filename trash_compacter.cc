6e3150f5605b5c1165c2b7deb63ff7980e1399c3
Matercan <matercan@proton.me>
2025-12-06 13:11:43 +0100

    Part one for trash compactor fixed

--- trash_compacter.cc
+++ trash_compacter.cc
@@ -3,7 +3,6 @@
 #include <boost/token_functions.hpp>
 #include <boost/tokenizer.hpp>
 
-#include <algorithm>
 #include <fstream>
 #include <print>
 #include <string>
@@ -34,7 +33,7 @@ int main(int argc, char *argv[]) {
     return 1;
 
   std::ifstream file(argv[1]);
-  std::vector<std::pair<bool, std::vector<long long>>> grid{};
+  std::vector<std::pair<bool, std::vector<int>>> grid{};
 
   FileSplit result{};
   {
@@ -60,55 +59,22 @@ int main(int argc, char *argv[]) {
   }
 
   {
-    // First tokenize each line into number strings
-    std::vector<std::vector<std::string>> rows{};
+    std::vector<int> line_input{};
     for (const auto &line : result.before) {
-      std::vector<std::string> line_tokens{};
-      tokenizeV(line, line_tokens);
-      rows.push_back(line_tokens);
-    }
+      tokenizeV(line, line_input);
 
-    // Find max number of columns
-    size_t max_cols = 0;
-    for (const auto &row : rows) {
-      max_cols = std::max(max_cols, row.size());
-    }
-
-    // Process columns RIGHT TO LEFT
-    for (int col = max_cols - 1; col >= 0; col--) {
-      // Collect all numbers in this column from all rows
-      std::vector<std::string> column_nums{};
-      for (const auto &row : rows) {
-        if ((size_t)col < row.size()) {
-          column_nums.push_back(row[col]);
+      // Generate a grid by splitting vertially instead of horizontally
+      for (size_t j = 0; j < line_input.size(); j++) {
+        if (grid.size() <= j) {
+          grid.push_back(
+              {true,
+               {line_input[j]}}); // True here represents having a plus operator
+        } else {
+          grid[j].second.push_back(line_input[j]);
         }
       }
 
-      // Find max digit length in this column
-      size_t max_len = 0;
-      for (const auto &num : column_nums) {
-        max_len = std::max(max_len, num.size());
-      }
-
-      // Read each digit position from RIGHT TO LEFT
-      std::vector<long long> problem_numbers{};
-      for (int digit_pos = max_len - 1; digit_pos >= 0; digit_pos--) {
-        std::string vertical_num = "";
-
-        // Read this digit position from all numbers in column (top to bottom)
-        for (const auto &num : column_nums) {
-          if ((size_t)digit_pos < num.size()) {
-            vertical_num += num[digit_pos];
-          }
-        }
-
-        if (!vertical_num.empty()) {
-          problem_numbers.push_back(
-              boost::lexical_cast<long long>(vertical_num));
-        }
-      }
-
-      grid.push_back({true, problem_numbers});
+      line_input.clear();
     }
   }
 
@@ -117,10 +83,8 @@ int main(int argc, char *argv[]) {
     for (const auto &line : result.after) {
       tokenizeV(line, operator_input);
 
-      // Operators are also right-to-left, so reverse them
-      std::reverse(operator_input.begin(), operator_input.end());
+      for (size_t j = 0; j < operator_input.size(); j++) {
 
-      for (size_t j = 0; j < operator_input.size() && j < grid.size(); j++) {
         if (operator_input[j] == '+')
           grid[j].first = true;
         else
